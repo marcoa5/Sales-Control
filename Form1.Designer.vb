03bded184ec8f637a12ce769458cedf3c6cd53ec
marcoa5 <58443501+marcoa5@users.noreply.github.com>
2020-02-01 09:48:46 +0100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,337 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Inizio = New System.Windows.Forms.DateTimePicker()
+        Me.Fine = New System.Windows.Forms.DateTimePicker()
+        Me.IYC = New System.Windows.Forms.CheckBox()
+        Me.GRC = New System.Windows.Forms.CheckBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox5 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox6 = New System.Windows.Forms.CheckBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.OOH = New System.Windows.Forms.CheckBox()
+        Me.GroupBox5 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox5.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Inizio
+        '
+        Me.Inizio.Location = New System.Drawing.Point(61, 22)
+        Me.Inizio.MinDate = New Date(2012, 1, 1, 0, 0, 0, 0)
+        Me.Inizio.Name = "Inizio"
+        Me.Inizio.Size = New System.Drawing.Size(200, 20)
+        Me.Inizio.TabIndex = 1
+        '
+        'Fine
+        '
+        Me.Fine.Location = New System.Drawing.Point(61, 48)
+        Me.Fine.Name = "Fine"
+        Me.Fine.Size = New System.Drawing.Size(200, 20)
+        Me.Fine.TabIndex = 3
+        '
+        'IYC
+        '
+        Me.IYC.AutoSize = True
+        Me.IYC.Location = New System.Drawing.Point(9, 28)
+        Me.IYC.Name = "IYC"
+        Me.IYC.Size = New System.Drawing.Size(50, 21)
+        Me.IYC.TabIndex = 5
+        Me.IYC.Text = "IYC"
+        Me.IYC.UseVisualStyleBackColor = True
+        '
+        'GRC
+        '
+        Me.GRC.AutoSize = True
+        Me.GRC.Location = New System.Drawing.Point(88, 26)
+        Me.GRC.Name = "GRC"
+        Me.GRC.Size = New System.Drawing.Size(56, 21)
+        Me.GRC.TabIndex = 6
+        Me.GRC.Text = "GRC"
+        Me.GRC.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.IYC)
+        Me.GroupBox1.Controls.Add(Me.GRC)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 92)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(188, 55)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Environment"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.CheckBox5)
+        Me.GroupBox2.Controls.Add(Me.CheckBox6)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 153)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(188, 55)
+        Me.GroupBox2.TabIndex = 20
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "SR-SI"
+        '
+        'CheckBox5
+        '
+        Me.CheckBox5.AutoSize = True
+        Me.CheckBox5.Location = New System.Drawing.Point(9, 28)
+        Me.CheckBox5.Name = "CheckBox5"
+        Me.CheckBox5.Size = New System.Drawing.Size(48, 21)
+        Me.CheckBox5.TabIndex = 5
+        Me.CheckBox5.Text = "SR"
+        Me.CheckBox5.UseVisualStyleBackColor = True
+        '
+        'CheckBox6
+        '
+        Me.CheckBox6.AutoSize = True
+        Me.CheckBox6.Location = New System.Drawing.Point(88, 28)
+        Me.CheckBox6.Name = "CheckBox6"
+        Me.CheckBox6.Size = New System.Drawing.Size(43, 21)
+        Me.CheckBox6.TabIndex = 6
+        Me.CheckBox6.Text = "SI"
+        Me.CheckBox6.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Button7)
+        Me.GroupBox3.Controls.Add(Me.Button6)
+        Me.GroupBox3.Controls.Add(Me.Button5)
+        Me.GroupBox3.Controls.Add(Me.Button4)
+        Me.GroupBox3.Controls.Add(Me.Label2)
+        Me.GroupBox3.Controls.Add(Me.Inizio)
+        Me.GroupBox3.Controls.Add(Me.Label1)
+        Me.GroupBox3.Controls.Add(Me.Fine)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(389, 74)
+        Me.GroupBox3.TabIndex = 1
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Start - End"
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(267, 22)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(50, 20)
+        Me.Button7.TabIndex = 18
+        Me.Button7.Text = "Day"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(323, 47)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(50, 20)
+        Me.Button6.TabIndex = 17
+        Me.Button6.Text = "FY+PY"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(323, 22)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(50, 20)
+        Me.Button5.TabIndex = 16
+        Me.Button5.Text = "Month"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(267, 48)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(50, 20)
+        Me.Button4.TabIndex = 15
+        Me.Button4.Text = "Year"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(26, 13)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "End"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "Start"
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.CheckOnClick = True
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Items.AddRange(New Object() {"MRS", "RDT", "REX", "RGU", "SED", "URE", "CTH", "CHS", "OTH"})
+        Me.CheckedListBox1.Location = New System.Drawing.Point(9, 49)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(180, 123)
+        Me.CheckedListBox1.TabIndex = 20
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.Button2)
+        Me.GroupBox4.Controls.Add(Me.CheckBox3)
+        Me.GroupBox4.Controls.Add(Me.CheckedListBox1)
+        Me.GroupBox4.Location = New System.Drawing.Point(206, 92)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(195, 173)
+        Me.GroupBox4.TabIndex = 40
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Business Lines"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(85, 23)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(69, 20)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "MR"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(9, 26)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(77, 21)
+        Me.CheckBox3.TabIndex = 1
+        Me.CheckBox3.Text = "Select All"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(325, 278)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(76, 30)
+        Me.Button1.TabIndex = 50
+        Me.Button1.Text = "Ok"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(239, 278)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(76, 30)
+        Me.Button3.TabIndex = 60
+        Me.Button3.Text = "Annulla"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'OOH
+        '
+        Me.OOH.AutoSize = True
+        Me.OOH.Location = New System.Drawing.Point(9, 19)
+        Me.OOH.Name = "OOH"
+        Me.OOH.Size = New System.Drawing.Size(95, 21)
+        Me.OOH.TabIndex = 18
+        Me.OOH.Text = "Include OOH"
+        Me.OOH.UseVisualStyleBackColor = True
+        '
+        'GroupBox5
+        '
+        Me.GroupBox5.Controls.Add(Me.OOH)
+        Me.GroupBox5.Location = New System.Drawing.Point(12, 214)
+        Me.GroupBox5.Name = "GroupBox5"
+        Me.GroupBox5.Size = New System.Drawing.Size(188, 51)
+        Me.GroupBox5.TabIndex = 30
+        Me.GroupBox5.TabStop = False
+        Me.GroupBox5.Text = "Orders On Hand"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(407, 314)
+        Me.Controls.Add(Me.GroupBox5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Sales Control Update"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.GroupBox5.ResumeLayout(False)
+        Me.GroupBox5.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Inizio As DateTimePicker
+    Friend WithEvents Fine As DateTimePicker
+    Friend WithEvents IYC As CheckBox
+    Friend WithEvents GRC As CheckBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents CheckBox6 As CheckBox
+    Private WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CheckedListBox1 As CheckedListBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents OOH As CheckBox
+    Friend WithEvents GroupBox5 As GroupBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,249 @@
+﻿Imports Excel = Microsoft.Office.Interop.Excel
+Imports System.Runtime.InteropServices
+Imports Microsoft.Office.Interop
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If System.Diagnostics.Debugger.IsAttached Then
+            Me.Text = "Sales Control Debug Mode"
+        Else
+            Me.Text = "Sales Control Version " & My.Application.Deployment.CurrentVersion.ToString
+        End If
+
+
+        Inizio.Value = "01/01/" & Year(Now())
+        Fine.Value = Now()
+        CheckBox3.Checked = True
+        IYC.Checked = True
+        GRC.Checked = True
+        CheckBox5.Checked = True
+        CheckBox6.Checked = True
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Testo As String = "select * from ""SalesControl"".""dbo"".""SalesControlAll"" Where TransactionDateISO >= " & Format(Inizio.Value, "yyyyMMdd") & " AND TransactionDateISO <= " & Format(Fine.Value, "yyyyMMdd")
+        Dim Env As String = ""
+        If IYC.Checked = True And GRC.Checked = False Then Env = "IYC"
+        If IYC.Checked = False And GRC.Checked = True Then Env = "GRC"
+        If Env <> "" Then Testo = Testo & " AND ""Environment"" = '" & Env & "'"
+
+        Dim SRSI As String = ""
+        If CheckBox5.Checked = True And CheckBox6.Checked = False Then SRSI = "SR"
+        If CheckBox5.Checked = False And CheckBox6.Checked = True Then SRSI = "SI"
+        If SRSI <> "" Then Testo = Testo & " AND ""SR-SI"" = '" & SRSI & "'"
+
+        Dim BL As String = " AND ("
+        Dim O As Integer = 0
+        For i As Integer = 0 To CheckedListBox1.CheckedItems.Count - 1
+            If O = 0 Then
+                BL = BL & """Business Line"" = '" & CheckedListBox1.CheckedItems(i).ToString & "'"
+            Else
+                BL = BL & " OR ""Business Line"" = '" & CheckedListBox1.CheckedItems(i).ToString & "'"
+            End If
+            O = 1
+        Next
+        BL &= ")"
+        Testo = Testo & " " & BL
+
+        'Excel
+
+        Dim mEx As Excel.Application
+        Dim mWb As Excel.Workbook
+        Dim msWb As Excel.Workbooks
+        Dim mWs As Excel.Worksheet
+        Dim mWs1 As Excel.Worksheet
+        mEx = CreateObject("Excel.Application")
+        mEx.Visible = True
+        msWb = mEx.Workbooks
+        mWb = msWb.Add
+        mWs = mWb.ActiveSheet
+        mWs.Name = "Sales Control"
+
+        mEx.WindowState = Microsoft.Office.Interop.Excel.XlWindowState.xlMaximized
+
+        'Add Connections
+        With mWb.PivotCaches.Create(SourceType:=Microsoft.Office.Interop.Excel.XlPivotTableSourceType.xlExternal)
+            .Connection = "OLEDB;Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=True;Initial Catalog=SalesControl;Data Source=IYCSITDB0001;Use Procedure for Prepare=1;Auto Translate=True;Packet Size=4096;Workstation ID=IYCLITC2D6PQ2;Use Encryption for Data=False;Tag with column collation when possible=False"
+            .CommandText = Testo
+            .MaintainConnection = True
+            .CreatePivotTable(mWs.Range("A1"), "SC1")
+        End With
+        For Each Conn In mWb.Connections
+            With Conn
+                .OLEDBConnection.BackgroundQuery = True
+                .OLEDBConnection.RefreshOnFileOpen = True
+                .Name = "SalesControl"
+                Exit For
+            End With
+
+        Next
+
+        With mWs.PivotTables("SC1").PivotFields("Business Line")
+            .Orientation = Microsoft.Office.Interop.Excel.XlPivotFieldOrientation.xlRowField
+            .Position = 1
+            .subtotals(1) = False
+        End With
+        With mWs.PivotTables("SC1").PivotFields("Year")
+            .Orientation = Microsoft.Office.Interop.Excel.XlPivotFieldOrientation.xlColumnField
+            .Position = 1
+            .subtotals(1) = False
+        End With
+        mWs.PivotTables("SC1").AddDataField(mWs.PivotTables("SC1").PivotFields("SalesPrice"), "NIS")
+        With mWs.PivotTables("SC1").PivotFields("NIS")
+            .NumberFormat = "#,##0.00 €"
+        End With
+        With mWs.PivotTables("SC1").PivotFields("SR-SI")
+            .Orientation = Microsoft.Office.Interop.Excel.XlPivotFieldOrientation.xlColumnField
+            .Position = 2
+            .subtotals(1) = False
+        End With
+        If (Me.IYC.Checked = True And Me.GRC.Checked = False) Or (Me.IYC.Checked = False And Me.GRC.Checked = True) Then
+        Else
+
+            With mWs.PivotTables("SC1").PivotFields("Environment")
+                .Orientation = Microsoft.Office.Interop.Excel.XlPivotFieldOrientation.xlPageField
+                .Position = 1
+            End With
+        End If
+        mWs.PivotTables("SC1").RowAxisLayout(Microsoft.Office.Interop.Excel.XlLayoutRowType.xlTabularRow)
+        mWs.PivotTables("SC1").RepeatAllLabels(Microsoft.Office.Interop.Excel.XlPivotFieldRepeatLabels.xlRepeatLabels)
+
+
+
+        With mWs.PivotTables("SC1")
+            .ColumnGrand = True
+            .RowGrand = False
+        End With
+
+        mWs.PivotTables("SC1").HasAutoFormat = False
+        mWs.Cells.ColumnWidth = 15
+
+        'OOH
+
+        If Me.OOH.Checked = True Then
+            mWs1 = mWb.Sheets.Add(, mWs)
+            mWs1.Name = "OOH"
+            Dim Con As String = ""
+            With mWb.PivotCaches.Create(SourceType:=Microsoft.Office.Interop.Excel.XlPivotTableSourceType.xlExternal)
+                .Connection = "OLEDB;DRIVER=SQL Server;SERVER=IYCSITDB0001;UID=ssccg;Trusted_Connection=Yes;APP=Microsoft Office 2016;WSID=SSILITD49GHC2;DATABASE=SalesControl"
+                If Me.IYC.Checked = True And Me.GRC.Checked = False Then Con = "SELECT * FROM SalesControl.dbo.OOHAll where ""Environment"" = 'IYC'"
+                If Me.IYC.Checked = False And Me.GRC.Checked = True Then Con = "SELECT * FROM SalesControl.dbo.OOHAll where ""Environment"" = 'GRC'"
+                If Me.IYC.Checked = False And Me.GRC.Checked = False Then Con = "SELECT * FROM SalesControl.dbo.OOHAll where ""Environment"" = 'IYC' or ""Environment"" = 'GRC'"
+                If Me.IYC.Checked = True And Me.GRC.Checked = True Then Con = "SELECT * FROM SalesControl.dbo.OOHAll where ""Environment"" = 'IYC' or ""Environment"" = 'GRC'"
+
+                .CommandText = Con & BL
+                .MaintainConnection = True
+                .CreatePivotTable(mWs1.Range("A1"), "OOH1")
+            End With
+            For Each Conn In mWb.Connections
+                If Conn.name <> "SalesControl" Then
+                    With Conn
+                        .OLEDBConnection.BackgroundQuery = True
+                        .OLEDBConnection.RefreshOnFileOpen = True
+                        .Name = "SalesControlOOH"
+                        Exit For
+                    End With
+                End If
+            Next
+            With mWs1.PivotTables("OOH1").PivotFields("Business Line")
+                .Orientation = Microsoft.Office.Interop.Excel.XlPivotFieldOrientation.xlRowField
+                .Position = 1
+                .subtotals(1) = False
+            End With
+            mWs1.PivotTables("OOH1").AddDataField(mWs1.PivotTables("OOH1").PivotFields("TotalSalesPrice"), "NIS")
+            With mWs1.PivotTables("OOH1").PivotFields("NIS")
+                .NumberFormat = "#,##0.00 €"
+            End With
+            With mWs1.PivotTables("OOH1").PivotFields("Environment")
+                .Orientation = Microsoft.Office.Interop.Excel.XlPivotFieldOrientation.xlPageField
+                .Position = 1
+                .subtotals(1) = False
+            End With
+            mWs1.PivotTables("OOH1").RowAxisLayout(Microsoft.Office.Interop.Excel.XlLayoutRowType.xlTabularRow)
+            mWs1.PivotTables("OOH1").RepeatAllLabels(Microsoft.Office.Interop.Excel.XlPivotFieldRepeatLabels.xlRepeatLabels)
+            With mWs1.PivotTables("OOH1")
+                .ColumnGrand = True
+                .RowGrand = False
+            End With
+            mWs1.PivotTables("OOH1").HasAutoFormat = False
+            mWs1.Cells.ColumnWidth = 15
+        End If
+
+        mWs.Select()
+        Me.Close()
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+        If CheckBox3.Checked = True Then
+            For i As Integer = 0 To CheckedListBox1.Items.Count - 1
+                CheckedListBox1.SetItemChecked(i, True)
+            Next
+        Else
+            For i As Integer = 0 To CheckedListBox1.Items.Count - 1
+                CheckedListBox1.SetItemChecked(i, False)
+            Next
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        CheckBox3.Checked = False
+        For I As Integer = 0 To CheckedListBox1.Items.Count - 1
+            If CheckedListBox1.Items.Item(I) = "SED" Or CheckedListBox1.Items.Item(I) = "URE" Or CheckedListBox1.Items.Item(I) = "RDT" Or CheckedListBox1.Items.Item(I) = "REX" Or CheckedListBox1.Items.Item(I) = "RGU" Or CheckedListBox1.Items.Item(I) = "MRS" Then
+                CheckedListBox1.SetItemChecked(I, True)
+            Else
+                CheckedListBox1.SetItemChecked(I, False)
+            End If
+        Next
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim I As New System.DateTime(Year(Now()), 1, 1)
+        Inizio.Value = I
+        Dim F As New System.DateTime(Year(Now()), 12, 31)
+        Fine.Value = F
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim mese As Integer = Month(Now())
+        Dim Giorno As Integer = 0
+        Select Case mese
+            Case 11, 4, 6, 9
+                Giorno = 30
+            Case 2
+                Giorno = 28
+            Case 1, 3, 5, 7, 8, 10, 12
+                Giorno = 31
+        End Select
+
+        Dim I As New System.DateTime(Year(Now()), Month(Now()), 1)
+        Inizio.Value = I
+        Dim F As New System.DateTime(Year(Now()), Month(Now()), Giorno)
+        Fine.Value = F
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim I As New System.DateTime(Year(Now()) - 1, 1, 1)
+        Inizio.Value = I
+        Dim F As New System.DateTime(Year(Now()), 12, 31)
+        Fine.Value = F
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim I As New System.DateTime(Year(Now()), Month(Now()), DateAndTime.Day(Now()) - 1)
+        Inizio.Value = I
+        Fine.Value = I
+    End Sub
+
+    Private Sub CheckedListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CheckedListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
